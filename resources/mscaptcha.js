3f91f61605718b063305301da5daf46e02c3184c
davchezt <davchezt@gmail.com>
2014-09-07 05:57:27 +0700

    Fix
    Fix not refresh img on FF

--- resources/mscaptcha.js
+++ resources/mscaptcha.js
@@ -1,9 +1,9 @@
 $('document').ready(function() {
-	$('form').on('click', '#mscaptcha-refresh', function () {
+	$('#mscaptcha-refresh').click(function (e) {
+		e.preventDefault();
 		$('.mscaptcha-loader').show();
 		$('.img-mscaptcha').load(function () {
 			$('.mscaptcha-loader').hide();
-		}).attr('src', ET.webPath + '/mscaptcha');
-		return false;
+		}).attr('src', ET.webPath + '/mscaptcha?' + (Math.random() * 5));
 	});
 });
